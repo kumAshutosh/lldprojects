ec4152b66f4723b860bd87cf0b40152511101c9d
Ashutosh Kumar <ashutosh09009@gmail.com>
2026-03-10 16:57:42 +0530

    removed unwanted folder

--- linkedinLearning/src/com/ashutosh/filemanager/UIEventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.ashutosh.filemanager;
-
-public interface UIEventHandler {
-
-	public void onCreate(String Path);
-	public void onList(String Path);
-	public void onDelete(String Path);
-}
